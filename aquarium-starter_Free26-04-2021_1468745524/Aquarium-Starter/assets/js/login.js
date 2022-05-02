11cd4ae71ad32d677e519dca76e9304f19a32969
andrés camilo gómez orozco <andresc.gomez@upb.edu.co>
2022-05-01 21:30:13 -0500

    agregamos js del login

--- aquarium-starter_Free26-04-2021_1468745524/Aquarium-Starter/assets/js/login.js
+++ aquarium-starter_Free26-04-2021_1468745524/Aquarium-Starter/assets/js/login.js
@@ -13,36 +13,6 @@ function iniciarLogin(){
     btn_cerrar_modal.addEventListener("click",cerrarVentana);
 }
 
-function mensaje_exito(texto_mensaje){
-    imagen.innerHTML = "🥳"; 
-    btn_cerrar_modal.innerHTML = "Continuar";
-    titulo.innerHTML = "Yayy!";
-    mensaje.innerHTML = texto_mensaje;
-    titulo.classList.remove("color_error_texto");
-    titulo.classList.add("color_exito_texto");
-    btn_cerrar_modal.classList.remove("color_error_texto","color_error_borde")
-    btn_cerrar_modal.classList.add("color_exito_texto","color_exito_borde");
-}
-
-function mensaje_error(texto_mensaje){
-    imagen.innerHTML = "😥"; 
-    btn_cerrar_modal.innerHTML = "Intenta nuevamente";
-    titulo.innerHTML = "Oops!";
-    mensaje.innerHTML = texto_mensaje;
-    titulo.classList.add("color_exito_texto");
-    titulo.classList.add("color_error_texto");
-    btn_cerrar_modal.classList.remove("color_exito_texto","color_exito_borde");
-    btn_cerrar_modal.classList.add("color_error_texto","color_error_borde");
-}
-
-function abrirVentana(){
-    ventana.classList.remove("hidden");
-}
-
-function cerrarVentana(evento){
-    //ventana.classList.add("hidden");
-    location.href = "index.html";
-}
 
 function procesarLogin(evento){
 
