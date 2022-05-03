c92f2324b56f8a745aec84e0b3f740c6f3337efc
andrés camilo gómez orozco <andresc.gomez@upb.edu.co>
2022-05-03 16:06:10 -0500

    actualizamos funcionamiento de login.html

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,9 @@ let correo,clave, usuario, imagen, titulo,mensaje;
 function iniciarLogin(){
     btn_enviar = document.getElementById("btn_enviar");
     btn_cerrar_modal = document.getElementById("btn_cerrar_modal");
-
+    mensaje = document.querySelector("#ventana p");
+    titulo = document.querySelector("#ventana h1");
+    imagen = document.querySelector("#ventana span");
     correo = document.getElementById("correo");
     clave = document.getElementById("clave");
     ventana = document.querySelector("#ventana");
@@ -13,6 +15,36 @@ function iniciarLogin(){
     btn_cerrar_modal.addEventListener("click",cerrarVentana);
 }
 
+function mensaje_exito(texto_mensaje){
+    imagen.innerHTML = "🥳"; 
+    btn_cerrar_modal.innerHTML = "Continuar";
+    titulo.innerHTML = "Yayy!";
+    mensaje.innerHTML = texto_mensaje;
+    titulo.classList.remove("color_error_texto");
+    titulo.classList.add("color_exito_texto");
+    btn_cerrar_modal.classList.remove("color_error_texto","color_error_borde")
+    btn_cerrar_modal.classList.add("color_exito_texto","color_exito_borde");
+}
+
+function mensaje_error(texto_mensaje){
+    imagen.innerHTML = "😥"; 
+    btn_cerrar_modal.innerHTML = "Intenta nuevamente";
+    titulo.innerHTML = "Oops!";
+    mensaje.innerHTML = texto_mensaje;
+    titulo.classList.add("color_exito_texto");
+    titulo.classList.add("color_error_texto");
+    btn_cerrar_modal.classList.remove("color_exito_texto","color_exito_borde");
+    btn_cerrar_modal.classList.add("color_error_texto","color_error_borde");
+}
+
+function abrirVentana(){
+    ventana.classList.remove("hidden");
+}
+
+function cerrarVentana(evento){
+    //ventana.classList.add("hidden");
+    location.href = "index.html";
+}
 
 function procesarLogin(evento){
 
